ea72ae22a9ac4f800c5b3784970914323552fa3d
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2021-10-08 23:57:44 +0300

    function and tests

---
+++ src/utils/__test__/checkBodyText.test.ts
@@ -0,0 +1,62 @@
+import { checkBodyText } from "../checkBodyText";
+
+describe("checkBodyText", () => {
+  test("should detect inline url", () => {
+    const res = checkBodyText("look at this: http://example.com/image123", [
+      "example.com",
+    ]);
+
+    expect(res).toBe(true);
+  });
+
+  test("should not detect inline url not from blacklisted domains", () => {
+    const res = checkBodyText("look at this: http://example.com/image123", [
+      "baddomain.com",
+    ]);
+
+    expect(res).toBe(false);
+  });
+
+  test("should not detect well formed urls", () => {
+    const res = checkBodyText("look at [this](http://example.com/image123)", [
+      "example.com",
+    ]);
+
+    expect(res).toBe(false);
+  });
+
+  test("should not detect images", () => {
+    const res = checkBodyText(
+      "look at this image ![](http://example.com/image123.jpg)",
+      ["example.com"]
+    );
+
+    expect(res).toBe(false);
+  });
+
+  test("should not detect code blocks", () => {
+    const res = checkBodyText(
+      `
+\`\`\`
+http://example.com/image123.jpg
+\`\`\`
+`,
+      ["example.com"]
+    );
+
+    expect(res).toBe(false);
+  });
+
+  test("should not detect html code blocks", () => {
+    const res = checkBodyText(
+      `
+<code>
+http://example.com/image123.jpg
+</code>
+`,
+      ["example.com"]
+    );
+
+    expect(res).toBe(false);
+  });
+});

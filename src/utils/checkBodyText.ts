cfdc6ae6c9bc9a63b7c5333172e2a9856a26b390
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2021-10-09 12:50:03 +0300

    detector

--- src/utils/checkBodyText.ts
+++ src/utils/checkBodyText.ts
@@ -1,12 +1,31 @@
-import { remark } from "remark";
+import remarkParse from "remark-parse";
+import { unified, Plugin } from "unified";
+import { visit } from "unist-util-visit";
 
-export async function checkBodyText(
-  body: string,
-  bannedDomains: string[]
-): Promise<boolean> {
-  const lexer = remark();
-  let tokens = lexer.parse(body).children;
+const plugin: Plugin = function (options = {}) {
+  const bannedDomains: string[] = options.bannedDomains || [];
 
-  // const res = remark().processSync(body);
-  return false;
+  this.Compiler = (markdownAST, file) => {
+    let found = false;
+    visit(markdownAST, "text", (node: any) => {
+      bannedDomains.forEach((bd) => {
+        if ((node.value as string).indexOf(bd) > -1) {
+          found = true;
+        }
+      });
+    });
+
+    return found ? "true" : "false";
+  };
+};
+
+export function checkBodyText(body: string, bannedDomains: string[]): boolean {
+  const processor = unified()
+    //
+    .use(remarkParse)
+    .use(plugin, { bannedDomains });
+
+  const res = processor.processSync(body);
+
+  return String(res) === "true";
 }
